555953837e1b7b8d94bf3b5ec26398b02044acd6
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2020-02-01 20:03:37 -0800

    Add useful menu itemms

--- mac-app/Tip/main.m
+++ mac-app/Tip/main.m
@@ -9,59 +9,30 @@
 #import <Cocoa/Cocoa.h>
 #import "Receiver.h"
 #import "ExternalTipper.h"
+#import "AppDelegate.h"
 
 #ifdef DEBUG
 #import "ForTest.h"
 #endif
 
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
     }
+        
+    #ifdef DEBUG
+        NSLog(@"DEBUG MODE: Register ForTest as a URL handler for tanintip://");
+        ForTest* forTest = [[ForTest alloc] init];
+        [forTest registerUrlSchemeForTest];
+    #endif
     
     NSApplication* app = NSApplication.sharedApplication;
-    
-    NSUserDefaults *args = [NSUserDefaults standardUserDefaults];
-    NSString* providerPath = [args stringForKey:@"provider"];
-    
-    if (!providerPath) {
-        NSURL *scriptsDirectory = [[NSFileManager defaultManager] URLForDirectory:NSApplicationScriptsDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:nil];
-        providerPath = [NSString stringWithFormat:@"%@/provider", scriptsDirectory.path];
-    }
- 
-    ExternalTipper *tipper = [[ExternalTipper alloc] initWithProvider:providerPath];
-    Receiver *receiver = [[Receiver alloc] initWithTipper:tipper];
-    receiver.controller = [[TipTableController alloc] init];
-    [NSApp setServicesProvider:receiver];
-
-    NSStatusBar* statusBar = NSStatusBar.systemStatusBar;
-    NSStatusItem *statusItem = [statusBar statusItemWithLength:25];
-    
-    statusItem.button.cell.font = [NSFont fontWithName:@"Font Awesome 5 Free" size:14];
-    statusItem.button.title = @"\uf05a";
-    statusItem.button.enabled = YES;
-    
-    statusItem.menu = [[NSMenu alloc] initWithTitle:@"Tip"];
-    [statusItem.menu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@""];
-    
-    NSString* testInput = [args stringForKey:@"test"];
-    if (testInput) {
-        NSPasteboard* pboard = [NSPasteboard pasteboardWithUniqueName];
-        [pboard setString:testInput forType:NSPasteboardTypeString];
-        NSString *error = nil;
-        [receiver openTips:pboard userData:@"" error:&error];
-    }
-    
-#ifdef DEBUG
-    NSLog(@"DEBUG MODE: Register ForTest as a URL handler for tanintip://");
-    ForTest* forTest = [[ForTest alloc] init];
-    [forTest registerUrlSchemeForTest];
-#endif
-    
+    AppDelegate *delegate = [[AppDelegate alloc] init];
+    app.delegate = delegate;
     [app run];
 }
 
 
-void terminate() {
-    [NSApp terminate:nil];
-}
+
+
