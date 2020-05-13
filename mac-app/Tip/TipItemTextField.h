9e756d71df492624f9351ead096c6d1607218195
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2020-05-12 22:32:23 -0700

    Manual layout (#48)

--- mac-app/Tip/TipItemTextField.h
+++ mac-app/Tip/TipItemTextField.h
@@ -13,6 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface TipItemTextField : NSTextField
 
 @property (nonatomic, retain) NSLayoutConstraint* widthConstraint;
+@property (nonatomic, retain) NSLayoutConstraint* heightConstraint;
 
 @end
 
